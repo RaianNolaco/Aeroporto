430066c3dc5d46b896239b2150f9e1d014eef450
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-05-23 21:36:41 -0300

    Update Administracao.java

--- Administracao.java
+++ Administracao.java
@@ -1,7 +1,6 @@
 // • A administração deve manter o cadastro de vôos e pode também localizar os aviões que estiverem em solo,
 // no aeroporto.
 
-
 public class Administracao {
     private Voo[] listaDeVoos = new Voo[1];
     private Aeronave[] aeronavesNoChao = new Aeronave[1];
@@ -27,5 +26,30 @@ public class Administracao {
         this.aeronavesNoChao = aeronavesNoChao;
     }
 
+    public void registrarVoo(Voo voo) {
+        aumentaCapacidadeListaDeVoo();
+        this.listaDeVoos[this.listaDeVoos.length - 1] = voo;
+    }
+
+    public void registraAeronave(Aeronave aeronave) {
+        aumentaCapacidadeAeronavesNoChao();
+        this.aeronavesNoChao[this.aeronavesNoChao.length - 1] = aeronave;
+    }
+
+    public void aumentaCapacidadeAeronavesNoChao() {
+        Aeronave[] temp = new Aeronave[this.aeronavesNoChao.length + 1];
+        for (int i = 0; i < this.aeronavesNoChao.length; i++) {
+            temp[i] = this.aeronavesNoChao[i];
+        }
+        this.aeronavesNoChao = temp;
+    }
+
+    public void aumentaCapacidadeListaDeVoo() {
+        Voo[] temp = new Voo[this.listaDeVoos.length + 1];
+        for (int i = 0; i < this.listaDeVoos.length; i++) {
+            temp[i] = this.listaDeVoos[i];
+        }
+        this.listaDeVoos = temp;
+    }
 
 }
