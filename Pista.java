7a9e47b18c02dc2957635c28625b845f5a82a4f4
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-05-23 21:29:24 -0300

    Update Pista.java

--- Pista.java
+++ Pista.java
@@ -5,17 +5,31 @@
 // pouso
 
 public class Pista {
+    private int id;
     private boolean livre;
 
-    public Pista() {
-    }
-
-    public boolean isLivre() {
-        return livre;
-    }
-
-    public void setLivre(boolean livre) {
+    public Pista(int id, boolean livre) {
+        this.id = id;
         this.livre = livre;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean estaLivre() {
+        return livre;
+    }
+
+    public void mudarEstado() {
+        if(this.livre){
+            this.livre = false;
+        }else{
+            this.livre = true;
+        }
+    }                                     
 }
